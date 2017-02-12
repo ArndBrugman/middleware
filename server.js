930cbe463f952f3bf17c90cd14b76dec0c03af6d
Arnd Brugman <arndbrugman@gmail.com>
2017-02-12 10:03:42 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,9 +1,29 @@
-var express = require('express')
-var app = express()
+var express = require('express');
+var http = require('http');
+var xml2js = require('xml2js');
+var parser = new xml2js.Parser();
+parser.on('error', function(err) { console.log('Parser error', err); });
+var app = express();
 app.set('port', process.env.OPENSHIFT_NODEJS_PORT || process.env.PORT || 8080);
 
 app.get('/', function (req, res) {
-  res.send('Hello World via port ' + app.get('port'))
+  // http://www.w3schools.com/xml/note.xml
+  // http://www.omdbapi.com/?t=Man&y=&plot=short&r=xml
+  var data = '';
+  http.get('http://www.omdbapi.com/?t=Man&y=&plot=short&r=xml', function(response) {
+    if (response.statusCode >= 200 && response.statusCode < 400) {
+      response.on('data', function(datablock) { 
+        data += datablock.toString(); 
+      });
+      res.on('end', function() {
+        console.log('data from request', data);
+        parser.parseString(data, function(err, result) {
+          res.send(result);
+          console.log('result as json', result);
+        });
+      });
+    }
+  });
 })
 
 app.listen(app.get('port'), function () {
