8fec247e6a57f701c8085fc8747fd7d6cc7836d9
Arnd Brugman <arndbrugman@gmail.com>
2017-02-14 14:17:40 +0100

    Update server.js

--- server.js
+++ server.js
@@ -29,5 +29,5 @@ app.get('/', function (req, res) {
 })
 
 app.listen(app.get('port'), function () {
-  console.log('Middleware server listening on port ' + app.get('port'))
+  console.log('Middleware server listening on port: ' + app.get('port'))
 })
